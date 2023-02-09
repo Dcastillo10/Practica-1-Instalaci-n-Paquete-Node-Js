b8530fb89e94c3029ffbde44f8418fb187e46ae6
Castillo <dcastillo10@ucol.mx>
2023-02-09 07:18:59 -0600

    first commit

---
+++ celsius/celsius.js
@@ -0,0 +1,8 @@
+let grados = require("celsius-fahrenheit-converter");
+console.log(grados.celsiusToFahrenheit(32));
+
+let grados2=require("celsius-fahrenheit-converter");
+console.log(grados.fahrenheitToCelcius(40));
+
+
+//celsiusToFahrenheit(19);
